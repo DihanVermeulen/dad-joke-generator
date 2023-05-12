6e595cb5f038380bc70e30d3f24c85e1ad413266
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-12 15:23:27 +0200

    Created app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <main></main>;
+  const [dadJoke, setDadJoke] = useState<string>("");
+
+  const fetchDadJoke = async () => {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    return response;
+  };
+  const handleFetchDadJoke = () => {
+    fetchDadJoke()
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the received joke data
+        setDadJoke(data.joke);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <main>
+      <h1>Dad Joke Generator</h1>
+      <button
+        onClick={() => {
+          handleFetchDadJoke();
+        }}
+      >
+        Random
+      </button>
+      <p>{dadJoke}</p>
+    </main>
+  );
 }
 
 export default App;
