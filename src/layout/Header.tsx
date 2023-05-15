47b359f4b254899ea3679351fb0c63efcb571ed3
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-15 10:22:19 +0200

    Created Header component

---
+++ src/layout/Header.tsx
@@ -0,0 +1,11 @@
+import InstallPWAButton from "../components/PwaInstallButton";
+
+const Header = () => {
+  return (
+    <header className="main__header">
+      <InstallPWAButton />
+    </header>
+  );
+};
+
+export { Header };
